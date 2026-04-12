cf2806cd7c6f2c1d8b86230ece3961807be2af8f
wisdhaus <wisdhaus@gmail.com>
2026-04-12 11:43:11 +0300

    Fixed coordinates of slot's graphic elements

--- Casino/Program.cs
+++ Casino/Program.cs
@@ -1,4 +1,6 @@
-﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+﻿using System.Numerics;
+
+Console.OutputEncoding = System.Text.Encoding.UTF8;
 Console.ForegroundColor = ConsoleColor.Cyan;
 
 //Console.WriteLine($"╔════════╗\n║⚡WILD⚡║\n║⚡WILD⚡║\n║⚡WILD⚡║\n╚════════╝");
@@ -24,76 +26,77 @@ void Output()
 Output();
 string[] Symbols = { "🇯", "🇶", "🇰", "🇹", "🥝", "🍋", "🍒", "💎", "🔔" };
 Random rand = new Random();
-Slot_With_Symbol Slot_1 = new Slot_With_Symbol(Symbols[rand.Next(0, 8)], 0, 0);
-Slot_1.Slot_Geometry_And_Output();
-Slot_With_Symbol Slot_2 = new Slot_With_Symbol(Symbols[rand.Next(0, 8)], 0, 0);
-Slot_2.Slot_Geometry_And_Output();
-Slot_With_Symbol Slot_3 = new Slot_With_Symbol(Symbols[rand.Next(0, 8)], 0, 0);
-Slot_3.Slot_Geometry_And_Output();
 
-struct Slot_With_Symbol(string symbol, int slot_x0, int slot_y0 = 0)
+Slot_With_Symbol Slot_1 = new Slot_With_Symbol(Symbols[rand.Next(0, 8)], 1, 0, 0);
+Slot_1.Slot_Characteristics();
+Slot_With_Symbol Slot_2 = new Slot_With_Symbol(Symbols[rand.Next(0, 8)], 1, 0, 0);
+Slot_2.Slot_Characteristics();
+Slot_With_Symbol Slot_3 = new Slot_With_Symbol(Symbols[rand.Next(0, 8)], 1, 0, 0);
+Slot_3.Slot_Characteristics();
+
+struct Slot_With_Symbol(string symbol, int scale, int slot_x0, int slot_y0 = 0)
 {
-    public static int scale = 1; // Scale of the interface
-    public static int slot_hight = 3 + 2 * scale;
-    public static int slot_width_vs_hight_ratio = 2;
-    public static int slot_width = slot_hight * slot_width_vs_hight_ratio;
-    public string[] Slot = new string[slot_hight * slot_width];
-    int[] Slot_X = new int[slot_hight * slot_width];
-    int[] Slot_Y = new int[slot_hight * slot_width];
-
-    public void Slot_Geometry_And_Output()
+    public void Slot_Characteristics()
     {
+        int slot_hight = 3 + 2 * scale;
+        int slot_width = slot_hight * 2;
+        int slot_square = slot_hight * slot_width;
+        string[] Slot_Graphic_Elements = new string[slot_square];
+        //Slot's Coordinates
+        int[] Slot_X = new int[slot_square];
+        int[] Slot_Y = new int[slot_square];
+
         for (int i = 0; i < slot_hight; i++)
         {
             for (int j = 0; j < slot_width; j++)
             {
-                Slot_X[j] = slot_x0 + j;
+                Slot_X[j + slot_width * i] = slot_x0 + j;
+                Slot_Y[j + slot_width * i] = slot_y0 + i;
             }
         }
-        for (int i = 0; i < slot_width; i++)
+
+        //Slot's Graphics
+        for (int i = 0; i < Slot_Graphic_Elements.Length; i++)
         {
-            for (int j = 0; j < slot_hight; j++)
+            if (i != Slot_Graphic_Elements.Length / 2)
             {
-                Slot_Y[j] = slot_y0 + j;
+                Slot_Graphic_Elements[i] = " ";
             }
         }
-        //Slot's geometry
-        for (int i = 0; i < Slot.Length; i++)
-        {
-            if (i != Slot.Length / 2)
-            {
-                Slot[i] = " ";
-            }
-        }
-        Slot[0] = "╔";
-        Slot[Slot.Length / slot_hight - 1] = "╗";
-        Slot[Slot.Length - slot_width] = "╚";
-        Slot[Slot.Length - 1] = "╝";
-        Slot[Slot.Length / 2 - 1] = symbol;
+        Slot_Graphic_Elements[0] = "╔";
+        Slot_Graphic_Elements[Slot_Graphic_Elements.Length / slot_hight - 1] = "╗";
+        Slot_Graphic_Elements[Slot_Graphic_Elements.Length - slot_width] = "╚";
+        Slot_Graphic_Elements[Slot_Graphic_Elements.Length - 1] = "╝";
+        Slot_Graphic_Elements[Slot_Graphic_Elements.Length / 2 - 1] = symbol;
         for (int i = 1; i < slot_width - 1; i++)
         {
-            Slot[i] = "═";
+            Slot_Graphic_Elements[i] = "═";
         }
-        for (int i = Slot.Length - slot_width + 1; i < Slot.Length - 1; i++)
+        for (
+            int i = Slot_Graphic_Elements.Length - slot_width + 1;
+            i < Slot_Graphic_Elements.Length - 1;
+            i++
+        )
         {
-            Slot[i] = "═";
+            Slot_Graphic_Elements[i] = "═";
         }
-        for (int i = slot_width; i < Slot.Length - slot_width; )
+        for (int i = slot_width; i < Slot_Graphic_Elements.Length - slot_width; )
         {
-            Slot[i] = "║";
+            Slot_Graphic_Elements[i] = "║";
             i = i + slot_width;
         }
-        for (int i = slot_width * 2 - 1; i < Slot.Length - slot_width; )
+        for (int i = slot_width * 2 - 1; i < Slot_Graphic_Elements.Length - slot_width; )
         {
-            Slot[i] = "║";
+            Slot_Graphic_Elements[i] = "║";
             i = i + slot_width;
         }
+
         //Slot's output
-        for (int i = 0; i < Slot.Length; )
+        for (int i = 0; i < Slot_Graphic_Elements.Length; )
         {
             for (int j = 0; j < slot_width; j++)
             {
-                Console.Write(Slot[i]);
+                Console.Write(Slot_Graphic_Elements[i]);
                 i++;
             }
             Console.WriteLine();
